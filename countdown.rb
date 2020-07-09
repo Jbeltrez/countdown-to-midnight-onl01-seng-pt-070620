1827618033fc4940ee2026e6fa1335faa5d16014
john beltrez <beltrezjohn@gmail.com>
2020-07-09 20:43:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
-def countdown(num)
-  while num > 0 
-  puts "#{num} SECOND(S)!"
-  num -= 1
-  if num == 0 
+def countdown(number)
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  if number == 0 
     "HAPPY NEW YEAR!"
   end
 end 
